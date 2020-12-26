eefb6b541314a2aebcf08bdda2ab6ccb3b2fc451
Mykhayl Puzanov <barbos@inbox.ru>
2020-12-26 18:59:13 +0300

    Object-oriented design start

--- src/ay-memory.ads
+++ src/ay-memory.ads
@@ -2,40 +2,43 @@ with Interfaces; use Interfaces;
 
 package Ay.Memory is
 
+   type T_BoolProxy is abstract tagged null record
+   type P_BoolProxy is access all T_BoolProxy'Class;
+
+   type T_Int16Proxy is abstract tagged null record
+   type P_Int16Proxy is access all T_Int16Proxy'Class;
+
+   type T_Int32Proxy is abstract tagged null record
+   type P_Int32Proxy is access all T_Int32Proxy'Class;
+
+   type T_Fp32Proxy is abstract tagged null record
+   type P_Fp32Proxy is access all T_Fp32Proxy'Class;
+
+   type T_Fp64Proxy is abstract tagged null record
+   type P_Fp64Proxy is access all T_Fp64Proxy'Class;
+
+   function Get(dp : P_BoolProxy) return Boolean;
+   procedure Set(dp : in out P_BoolProxy; value : Boolean);
+
+   function Get(dp : P_Int16Proxy) return Integer_16;
+   procedure Set(dp : in out P_Int16Proxy; value : Integer_16);
+
+   function Get(dp : P_Int32Proxy) return Integer_32;
+   procedure Set(dp : in out P_Int32Proxy; value : Integer_32);
+
+   function Get(dp : P_Fp32Proxy) return Float;
+   procedure Set(dp : in out P_Fp32Proxy; value : Float);
+
+   function Get(dp : P_Fp64Proxy) return Long_Float;
+   procedure Set(dp : in out P_Fp64Proxy; value : Long_Float);
+
    type T_MemoryAllocator is abstract tagged null record;
+   type P_MemoryAllocator is access all T_MemoryAllocator'Class;
 
-   function newInt(ma : in out T_MemoryAllocator'Class) return P_Int;
-   function newReal(ma : in out T_MemoryAllocator'Class) return P_Real;
-   function newBool(ma : in out T_MemoryAllocator'Class) return P_Bool;
-
-   function allocInt(ma : in out T_MemoryAllocator) return P_Int;
-   function allocReal(ma : in out T_MemoryAllocator) return P_Real;
-   function allocBool(ma : in out T_MemoryAllocator) return P_Bool;
-
-   subtype T_CellIndex is Unsigned_32;
-
-   type T_ArrayInt is array (T_CellIndex range <>) of aliased T_Int;
-   type T_ArrayReal is array (T_CellIndex range <>) of aliased T_Real;
-   type T_ArrayBool is array (T_CellIndex range <>) of aliased T_Bool;
-
-   type P_ArrayInt is access T_ArrayInt;
-   type P_ArrayReal is access T_ArrayReal;
-   type P_ArrayBool is access T_ArrayBool;
-
-   type T_MemoryAllocatorS is new T_MemoryAllocator with
-      record
-         di : P_ArrayInt;
-         dr : P_ArrayReal;
-         db : P_ArrayBool;
-         ipt : T_CellIndex;
-         rpt : T_CellIndex;
-         bpt : T_CellIndex;
-      end record;
-
-   procedure init(ma : in out T_MemoryAllocatorS; si, sr, sb: T_CellIndex);
-   function allocBool(ma : in out T_MemoryAllocatorS) return P_Bool;
-   function allocInt(ma : in out T_MemoryAllocatorS) return P_Int;
-   function allocReal(ma : in out T_MemoryAllocatorS) return P_Real;
-
+   function newBool(ma : P_MemoryAllocator) return P_BoolProxy;
+   function newInt16(ma : P_MemoryAllocator) return P_Int16Proxy;
+   function newInt32(ma : P_MemoryAllocator) return P_Int32Proxy;
+   function newFp32(ma : P_MemoryAllocator) return P_Fp32Proxy;
+   function newFp64(ma : P_MemoryAllocator) return P_Fp64Proxy;
 
 end Ay.Memory;
